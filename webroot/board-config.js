7071f80d154fec343b0f8ff4079c2bd8f22f2e13
Vitaliy Matiyash <vitaliy.matiyash@alliancedata.com>
2018-11-05 23:34:27 -0500

    initial commit for Istanbul MeasureCamp

---
+++ webroot/board-config.js
@@ -0,0 +1,8 @@
+gOptions = {
+  // This can have nested stuff, arrays, etc.
+  enabled: true,
+  city: 'Istanbul',
+  venue_name: '',
+  venue_address: '',
+  google_sheet_url: 'https://docs.google.com/spreadsheets/d/' + '19hR4zzI7eWN4n75vFRB_u1-lS6Hsst3FsC4NbEYbbok' + '/edit?usp=sharing'
+}
